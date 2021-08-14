7ff78cd69c8444c867eba361fded926ace6bd36f
rodolfomayora <rodolfomayora96@gmail.com>
2021-08-14 13:23:27 -0400

    chore: add page thumbnail

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -6,8 +6,9 @@ class MyDocument extends Document {
       <Html lang="es">
         <Head>
           <meta charSet="UTF-8" />
-          <meta name="keywords" content="rodolfo, mayora, pereda, FontEnd, Developer, Desarrollador, web, ReactJS, React" />
+          <meta name="keywords" content="rodolfo, mayora, pereda, FontEnd, Front-end, Developer, Desarrollador, Programador, Software, web, ReactJS, React, TypeScript, JavaScript" />
           <meta name="theme-color" content="hsla(0deg, 0%, 0%, 0)" />
+          <meta name="thumbnail" content="/thumbnail.png" />
           <link rel="icon" href="/favicon2.ico" />
         </Head>
         <body>
@@ -15,7 +16,7 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
