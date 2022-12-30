2880533d322c609c0fb36e2bcd28052522276fda
Rodolfo Mayora <rodolfomayora96@gmail.com>
2022-12-30 02:49:38 -0400

    shore: update assets

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,7 +1,9 @@
 import { AppProps } from 'next/app';
+import '../styles/styles.global.scss';
 
 import GlobalStyles from '../styles/GlobalStyles';
 import { SplashScreen } from '../components';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
