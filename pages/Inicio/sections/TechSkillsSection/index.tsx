79eaca13c2ef2327bed64a01065a37973d0709c6
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-01-16 21:44:49 -0400

    feat: isolated home page section components

---
+++ pages/Inicio/sections/TechSkillsSection/index.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import { technologies } from '../../../../data';
+import { addCustomId } from '../../../../utils';
+import {
+  PageSection,
+  TechnologyBlock,
+} from '../../../../components';
+import styles from './styles.module.scss';
+
+const TechSkillsSection: FC = () => {
+  const myTechSkills: Array<any> = technologies.map(addCustomId);
+
+  return (
+    <PageSection whiteColor>
+      <h2 className={styles.subtitle}>¿Qué utilizo?</h2>
+      
+      <div className={styles.technologyGrid}>
+        {myTechSkills?.map(technology => (
+          <TechnologyBlock
+            key={technology.customId}
+            name={technology.name}
+            pathIcon={technology.image}/>
+        ))}
+      </div>
+    </PageSection>
+  );
+}
+
+export default TechSkillsSection;
