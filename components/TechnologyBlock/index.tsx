3f83cd4846faf7abed61fe34e75aa82fae805b20
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-04-13 23:51:07 -0400

    style: modify layout for tech skills section

--- components/TechnologyBlock/index.tsx
+++ components/TechnologyBlock/index.tsx
@@ -8,7 +8,10 @@ const TecnologyBlock: FC<TechnologyBlockProps> = ({ name, pathIcon }) => (
       <img
         className={styles.logo}
         src={pathIcon}
-        alt={name + ' Icon'} />
+        alt={name + ' Icon'}
+        width={100}
+        height={100}
+      />
     </div>
     <div className={styles.label}>{name}</div>
   </div>
