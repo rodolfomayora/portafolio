93b16b10b1accf310b3468b122db312e1f930a43
Rodolfo Mayora <rodolfomayora96@gmail.com>
2023-04-13 23:50:02 -0400

    chore: update portfolio data

--- data/technologies.ts
+++ data/technologies.ts
@@ -1,11 +1,19 @@
 const tecnologies: Array<any> = [
   {
-    name: 'ReactJS',
+    name: 'Next.js',
+    image: '/images/svg/nextjs.svg'
+  },
+  {
+    name: 'React.js',
     image: '/images/svg/react.svg'
   },
   {
-    name: 'NextJS',
-    image: '/images/svg/nextjs.svg'
+    name: 'Vite.js',
+    image: '/images/svg/vitejs.svg'
+  },
+  {
+    name: 'Node.js',
+    image: '/images/svg/node.svg'
   },
   {
     name: 'TypeScript',
@@ -16,30 +24,38 @@ const tecnologies: Array<any> = [
     image: '/images/svg/javascript.svg'
   },
   {
-    name: 'Sass',
-    image: '/images/svg/sass.svg'
-  },
-  {
     name: 'Redux',
     image: '/images/svg/redux.svg'
   },
   {
-    name: 'Redux Saga',
-    image: '/images/svg/redux-saga.svg'
-  },
-  {
-    name: 'HTML5',
-    image: '/images/svg/html5.svg'
+    name: 'Sass/scss',
+    image: '/images/svg/sass.svg'
   },
   {
     name: 'CSS3',
-    image: '/images/svg/css3.svg'
+    image: '/images/svg/css.svg'
+  },
+  {
+    name: 'HTML5',
+    image: '/images/svg/html.svg'
+  },
+  {
+    name: 'Docker',
+    image: '/images/svg/docker.svg'
+  },
+  {
+    name: 'VSCode',
+    image: '/images/svg/vscode.svg'
+  },
+  {
+    name: 'WSL2',
+    image: '/images/svg/ubuntu.svg'
   },
   {
     name: 'Git',
     image: '/images/svg/git.svg'
   },
- 
+  
   // {
   //   name: '',
   //   image: ''
