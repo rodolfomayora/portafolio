dc161b25f8b432c3e4ff972aa2db21cd69ee8526
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-04-28 15:03:21 -0400

    fix(components): solve render issue on browser tab title

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -7,19 +7,22 @@ import Footer from '../Footer';
 import LayoutProps from './types';
 import styles from './styles.module.scss';
 
-const Layout: FC<LayoutProps> = ({ children, pageTitle }) => (
-  <>
-    <Head>
-      <title>{pageTitle}{' '}| Rodolfo Mayora Pereda | FrontEnd Developer</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="Portafolio de Rodolfo Mayora Pereda, FrontEnd Developer enfocado en ReactJS y tecnologías relacionadas, ingresa al portafolio si deseas saber más." />
-    </Head>
-    <Header currentPage={pageTitle} />
-    <div className={styles.pageContent}>
-      {children}
-    </div>
-    <Footer />
-  </>
-)
+const Layout: FC<LayoutProps> = ({ children, pageTitle }) => {
+  const title = `${pageTitle} | Rodolfo Mayora Pereda | FrontEnd Develope`;
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Portafolio de Rodolfo Mayora Pereda, FrontEnd Developer enfocado en ReactJS y tecnologías relacionadas, ingresa al portafolio si deseas saber más." />
+      </Head>
+      <Header currentPage={pageTitle} />
+      <div className={styles.pageContent}>
+        {children}
+      </div>
+      <Footer />
+    </>
+  );
+}
 
 export default Layout;
