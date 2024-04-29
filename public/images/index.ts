895e7d1b1a94a14b688382ef1e594dfe106fe0c1
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-04-29 11:08:16 -0400

    chore(public): update icons

--- public/images/index.ts
+++ public/images/index.ts
@@ -5,3 +5,4 @@ export { default as Github } from './svg/github.svg';
 export { default as Linkedin } from './svg/linkedin.svg';
 export { default as Telegram } from './svg/telegram.svg';
 export { default as Nextjs } from './svg/nextjs.svg';
+export { default as DockerFlat } from './svg/docker-flat.svg';
