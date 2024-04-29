b39667fb458832334bace08abec01d0506ba77d6
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-04-29 16:32:02 -0400

    feat(src): add custom hook for i18n

---
+++ src/hooks/useLocaleDictionary/index.ts
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router';
+import { en } from '#locales/en';
+import { es } from '#locales/es';
+
+export function useLocaleDictionary () {
+  const { locale } = useRouter();
+  const [baseLocale] = locale.split('-');
+  const isDefaultLocale = baseLocale === 'en';
+  const t = isDefaultLocale ? en : es;
+  return { t };
+}
