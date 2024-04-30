2546aad43e5a9e365eb5035a5585be4ce93516b4
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-04-30 15:49:38 -0400

    refactor(src): update social links implmentation

--- src/components/SocialLinks/index.tsx
+++ src/components/SocialLinks/index.tsx
@@ -6,33 +6,32 @@ import {
 } from '#icons/svg';
 import * as links from '#data/socialLinks';
 import { ExternalLink } from '#components/ExternalLink';
-import styles from './styles.module.scss';
 
 export function SocialLinks () {
   return (
     <>
-      <ExternalLink className={styles.ExternalLink}
+      <ExternalLink
         href={links.githubProfile}
         aria-label="github profile page"  
       >
         <GithubIcon />
       </ExternalLink>
 
-      <ExternalLink className={styles.ExternalLink}
+      <ExternalLink
         href={links.dockerhubProfile}
         aria-label="dockerhub profile page"    
       >
         <DockerFlatIcon />
       </ExternalLink>
   
-      <ExternalLink className={styles.ExternalLink}
+      <ExternalLink
         href={links.linkedInProfile}
         aria-label="linkedin profile page"  
       >
         <LinkedinIcon />
       </ExternalLink>
   
-      <ExternalLink className={styles.ExternalLink}
+      <ExternalLink
         href={links.telegramProfile}
         aria-label="telegram contact link"
       >
