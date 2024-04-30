f7f57c0bd313dcd34f4daa1acb42eda2ef6fffe5
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-04-30 14:36:01 -0400

    chore(public): move public icons location

--- public/images/index.ts
+++
@@ -1,8 +0,0 @@
-// Static
-
-// SVG
-export { default as Github } from './svg/github.svg';
-export { default as Linkedin } from './svg/linkedin.svg';
-export { default as Telegram } from './svg/telegram.svg';
-export { default as Nextjs } from './svg/nextjs.svg';
-export { default as DockerFlat } from './svg/docker-flat.svg';
