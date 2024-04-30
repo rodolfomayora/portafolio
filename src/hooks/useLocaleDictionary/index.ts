7383a3ded34be7a74d4a6fa6152edf8fde58f95d
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-04-30 15:18:15 -0400

    feat(hooks): use locale dictionary now returns pathname value

--- src/hooks/useLocaleDictionary/index.ts
+++ src/hooks/useLocaleDictionary/index.ts
@@ -3,12 +3,13 @@ import { en } from '#locales/en';
 import { es } from '#locales/es';
 
 export function useLocaleDictionary () {
-  const { locale } = useRouter();
+  const { locale, pathname } = useRouter();
   const [baseLocale] = locale.split('-');
   const isDefaultLocale = baseLocale === 'en';
   const t = isDefaultLocale ? en : es;
   return {
     t,
     baseLocale,
+    pathname,
   };
 }
