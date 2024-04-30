af5e3a56db94764a0575c7b642cf3352ccd0bf79
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-04-30 17:06:56 -0400

    chore(pages): remove depregated keywords meta tag

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -6,7 +6,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="UTF-8" />
-          {/* <meta name="keywords" content="rodolfo, mayora, pereda, FontEnd, Front-end, Developer, Desarrollador, Programador, Software, web, ReactJS, React, TypeScript, JavaScript" /> */}
           <meta name="theme-color" content="hsla(0deg, 0%, 0%, 0)" />
           <meta name="thumbnail" content="/thumbnail.png" />
           <link rel="icon" href="/favicon2.ico" />
