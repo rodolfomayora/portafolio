a30387dcfdba51258226b4e0b44b9e6ca4a874be
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-05-17 11:32:08 -0400

    chore(root): changes

--- src/components/Header/useScrollState.ts
+++ src/components/Header/useScrollState.ts
@@ -1,53 +1,53 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 
 
-type ScrollState = {
-  didScroll: boolean,
-}
+// type ScrollState = {
+//   didScroll: boolean,
+// }
 
-export function useScrollState (): ScrollState {
-  const [didScroll, setDidScroll] = useState<boolean>(false);
+// export function useScrollState (): ScrollState {
+//   const [didScroll, setDidScroll] = useState<boolean>(false);
 
-  // useEffect(() => {
-  //   const initialPoint = 0;
-  //   let isScrolling = false;
-  //   let isInTop = true;
+//   // useEffect(() => {
+//   //   const initialPoint = 0;
+//   //   let isScrolling = false;
+//   //   let isInTop = true;
 
-  //   const didUserScroll = window.pageYOffset > 0;
-  //   if (didUserScroll) setDidScroll(true);
+//   //   const didUserScroll = window.pageYOffset > 0;
+//   //   if (didUserScroll) setDidScroll(true);
 
-  //   const intervalWatcher = window.setInterval(() => {
-  //     if (isScrolling) {
-  //       // console.log('dddd');
-  //       isScrolling = false;
-  //       // const scrollYOffset = window.pageYOffset;
-  //       const scrollYOffset = window.scrollY;
-  //       const isAtInitialPoint = scrollYOffset === initialPoint;
-  //       const isNoAtInitialPoint = scrollYOffset > 0 && isInTop;
+//   //   const intervalWatcher = window.setInterval(() => {
+//   //     if (isScrolling) {
+//   //       // console.log('dddd');
+//   //       isScrolling = false;
+//   //       // const scrollYOffset = window.pageYOffset;
+//   //       const scrollYOffset = window.scrollY;
+//   //       const isAtInitialPoint = scrollYOffset === initialPoint;
+//   //       const isNoAtInitialPoint = scrollYOffset > 0 && isInTop;
 
-  //       if (isAtInitialPoint) {
-  //         isInTop = true;
-  //         setDidScroll(false);
-  //       }
+//   //       if (isAtInitialPoint) {
+//   //         isInTop = true;
+//   //         setDidScroll(false);
+//   //       }
 
-  //       if (isNoAtInitialPoint) {
-  //         isInTop = false;
-  //         setDidScroll(true);
-  //       }
-  //     }
-  //   }, 200);
+//   //       if (isNoAtInitialPoint) {
+//   //         isInTop = false;
+//   //         setDidScroll(true);
+//   //       }
+//   //     }
+//   //   }, 200);
 
-  //   const setScrollTrue = (): void => {
-  //     isScrolling = true;
-  //   };
+//   //   const setScrollTrue = (): void => {
+//   //     isScrolling = true;
+//   //   };
 
-  //   document.addEventListener('scroll', setScrollTrue);
+//   //   document.addEventListener('scroll', setScrollTrue);
 
-  //   return () => {
-  //     window.clearInterval(intervalWatcher);
-  //     document.removeEventListener('scroll', setScrollTrue);
-  //   }
-  // }, []);
+//   //   return () => {
+//   //     window.clearInterval(intervalWatcher);
+//   //     document.removeEventListener('scroll', setScrollTrue);
+//   //   }
+//   // }, []);
 
-  return { didScroll };
-}
+//   return { didScroll };
+// }
