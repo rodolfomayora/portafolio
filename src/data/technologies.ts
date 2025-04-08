4e4502035be1c7b50822f73821b4be44021af97a
Rodolfo Mayora <rodolfomayora96@gmail.com>
2025-04-08 03:03:10 -0400

    feat(src): update tech stack

--- src/data/technologies.ts
+++ src/data/technologies.ts
@@ -1,4 +1,5 @@
-export const technologies: { name: string, imageSource: string }[] = [
+export const technologies: { name: string, imageSource: string, description?: string }[] = [
+  // Frontend Technologies
   {
     name: 'HTML',
     imageSource: '/icons/svg/html.svg',
@@ -8,83 +9,115 @@ export const technologies: { name: string, imageSource: string }[] = [
     imageSource: '/icons/svg/css.svg',
   },
   {
-    name: 'JavaScript',
-    imageSource: '/icons/svg/javascript.svg',
-  },
-  {
     name: 'Sass/SCSS',
     imageSource: '/icons/svg/sass.svg',
+  },
+  {
+    name: 'JavaScript',
+    imageSource: '/icons/svg/javascript.svg',
   },
   {
     name: 'TypeScript',
     imageSource: '/icons/svg/typescript.svg',
   },
   {
-    name: 'Next.js',
-    imageSource: '/icons/svg/nextjs.svg',
-  },
-  {
     name: 'React.js',
     imageSource: '/icons/svg/react.svg',
   },
   {
-    name: 'Redux',
-    imageSource: '/icons/svg/redux.svg',
+    name: 'Next.js',
+    imageSource: '/icons/svg/nextjs.svg',
   },
-  // {
-  //   name: 'React Query',
-  //   imageSource: '/icons/svg/react-query.svg',
-  // },
   {
-    name: 'Tanstack Query',
-    imageSource: '/icons/svg/tanstack-query.png',
+    name: 'Redux Toolkit',
+    imageSource: '/icons/svg/redux.svg',
   },
   {
     name: 'Vite.js',
     imageSource: '/icons/svg/vitejs.svg',
   },
-  {
-    name: 'Jest',
-    imageSource: '/icons/svg/jest.svg',
-  },
-  {
-    name: 'Vitest',
-    imageSource: '/icons/svg/vitest.svg',
-  },
-  {
-    name: 'Testing Library',
-    imageSource: '/icons/svg/testing-library.svg',
-  },
-  // {
-  //   name: 'Husky.js',
-  //   imageSource: '/icons/svg/husky.svg',
-  // },
-  {
-    name: 'PNPM',
-    imageSource: '/icons/svg/pnpm.svg',
-  },
-  {
-    name: 'Corepack',
-    imageSource: '/icons/svg/corepack.svg',
-  },
+  // Backend Technologies
   {
     name: 'Node.js',
     imageSource: '/icons/svg/node.svg',
   },
   {
+    name: 'Express.js',
+    imageSource: '/icons/svg/expressjs.svg',
+  },
+  {
+    name: 'Nest.js',
+    imageSource: '/icons/svg/nestjs.svg',
+  },
+  {
+    name: 'PostgreSQL',
+    imageSource: '/icons/svg/postgresql.svg',
+  },
+  {
+    name: 'MongoDB',
+    imageSource: '/icons/svg/mongodb.svg',
+  },
+  // DevOPs & Tools
+  {
     name: 'Docker',
     imageSource: '/icons/svg/docker.svg',
   },
+  // {
+  //   name: 'Corepack',
+  //   imageSource: '/icons/svg/corepack.svg',
+  //   description: 'Manages multiple package managers efficiently.',
+  // },
   {
-    name: 'VSCode',
-    imageSource: '/icons/svg/vscode.svg',
+    name: 'Git',
+    imageSource: '/icons/svg/git.svg',
   },
   {
     name: 'Ubuntu',
     imageSource: '/icons/svg/ubuntu.svg',
   },
+  // Testing & Debugging
+  // {
+  //   name: 'Jest',
+  //   imageSource: '/icons/svg/jest.svg',
+  //   description: 'JavaScript testing framework for unit tests.',
+  // },
+  // {
+  //   name: 'Vitest',
+  //   imageSource: '/icons/svg/vitest.svg',
+  // },
+  // {
+  //   name: 'Testing Library',
+  //   imageSource: '/icons/svg/testing-library.svg',
+  // },
+  // {
+  //   name: 'Mock Service Worker',
+  //   imageSource: '/icons/svg/mswjs.svg',
+  //   description: 'Mocks API calls during testing.'
+  // },
+  // {
+  //   name: 'Playwright',
+  //   imageSource: '/icons/svg/playwright.svg',
+  //   description: 'End-to-end testing framework.',
+  // },
+  // Miscellaneous Tools
   {
-    name: 'Git',
-    imageSource: '/icons/svg/git.svg',
+    name: 'VSCode',
+    imageSource: '/icons/svg/vscode.svg',
+    description: 'Widely-used code editor.'
   },
+  {
+    name: 'Dbeaver DB Client',
+    imageSource: '/icons/svg/dbeaver.svg',
+    description: 'Tool for Database Management.'
+  },
+  {
+    name: 'Bruno HTTP Client',
+    imageSource: '/icons/svg/bruno.svg',
+    description: 'Tool for API testing.'
+  },
+  // {
+  //   name: 'Husky.js',
+  //   imageSource: '/icons/svg/husky.svg',
+  //   description: 'Git hooks manager to enforce rules.',
+  // },
 ];
