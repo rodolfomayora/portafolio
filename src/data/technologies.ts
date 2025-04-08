58c5954919706874553a1188e176a1d4556e2b91
Rodolfo Mayora <rodolfomayora96@gmail.com>
2025-04-08 11:48:48 -0400

    feat(src): reimplemented kills section with sliders

--- src/data/technologies.ts
+++ src/data/technologies.ts
@@ -1,123 +1,151 @@
-export const technologies: { name: string, imageSource: string, description?: string }[] = [
+type Category = 'frontend' | 'backend' | 'tool' | 'testing'
+export const technologies: { name: string, imageSource: string, description?: string, category: Category }[] = [
   // Frontend Technologies
   {
     name: 'HTML',
     imageSource: '/icons/svg/html.svg',
+    category: 'frontend',
   },
   {
     name: 'CSS',
     imageSource: '/icons/svg/css.svg',
+    category: 'frontend',
   },
   {
     name: 'Sass/SCSS',
     imageSource: '/icons/svg/sass.svg',
+    category: 'frontend',
   },
   {
     name: 'JavaScript',
     imageSource: '/icons/svg/javascript.svg',
+    category: 'frontend',
   },
   {
     name: 'TypeScript',
     imageSource: '/icons/svg/typescript.svg',
+    category: 'frontend',
   },
   {
     name: 'React.js',
     imageSource: '/icons/svg/react.svg',
+    category: 'frontend',
   },
   {
     name: 'Next.js',
     imageSource: '/icons/svg/nextjs.svg',
+    category: 'frontend',
   },
   {
     name: 'Redux Toolkit',
     imageSource: '/icons/svg/redux.svg',
+    category: 'frontend',
   },
   {
     name: 'Vite.js',
     imageSource: '/icons/svg/vitejs.svg',
+    category: 'frontend',
   },
   // Backend Technologies
   {
     name: 'Node.js',
     imageSource: '/icons/svg/node.svg',
+    category: 'backend',
   },
   {
     name: 'Express.js',
     imageSource: '/icons/svg/expressjs.svg',
+    category: 'backend',
   },
   {
     name: 'Nest.js',
     imageSource: '/icons/svg/nestjs.svg',
+    category: 'backend',
   },
   {
     name: 'PostgreSQL',
     imageSource: '/icons/svg/postgresql.svg',
+    category: 'backend',
   },
   {
     name: 'MongoDB',
     imageSource: '/icons/svg/mongodb.svg',
+    category: 'backend',
   },
   // DevOPs & Tools
   {
     name: 'Docker',
     imageSource: '/icons/svg/docker.svg',
+    category: 'tool',
   },
   // {
   //   name: 'Corepack',
   //   imageSource: '/icons/svg/corepack.svg',
+  //   category: '',
   //   description: 'Manages multiple package managers efficiently.',
   // },
   {
     name: 'Git',
     imageSource: '/icons/svg/git.svg',
+    category: 'tool',
   },
   {
     name: 'Ubuntu',
     imageSource: '/icons/svg/ubuntu.svg',
+    category: 'tool',
   },
   // Testing & Debugging
   // {
   //   name: 'Jest',
   //   imageSource: '/icons/svg/jest.svg',
+  //   category: 'testing',
   //   description: 'JavaScript testing framework for unit tests.',
   // },
   // {
   //   name: 'Vitest',
   //   imageSource: '/icons/svg/vitest.svg',
+  //   category: 'testing',
   // },
   // {
   //   name: 'Testing Library',
   //   imageSource: '/icons/svg/testing-library.svg',
+  //   category: 'testing',
   // },
   // {
   //   name: 'Mock Service Worker',
   //   imageSource: '/icons/svg/mswjs.svg',
+  //   category: 'testing',
   //   description: 'Mocks API calls during testing.'
   // },
   // {
   //   name: 'Playwright',
   //   imageSource: '/icons/svg/playwright.svg',
+  //   category: 'testing',
   //   description: 'End-to-end testing framework.',
   // },
   // Miscellaneous Tools
   {
     name: 'VSCode',
     imageSource: '/icons/svg/vscode.svg',
+    category: 'tool',
     description: 'Widely-used code editor.'
   },
   {
     name: 'Dbeaver DB Client',
     imageSource: '/icons/svg/dbeaver.svg',
+    category: 'tool',
     description: 'Tool for Database Management.'
   },
   {
     name: 'Bruno HTTP Client',
     imageSource: '/icons/svg/bruno.svg',
+    category: 'tool',
     description: 'Tool for API testing.'
   },
   // {
   //   name: 'Husky.js',
   //   imageSource: '/icons/svg/husky.svg',
+  //   category: 'tool',
   //   description: 'Git hooks manager to enforce rules.',
   // },
 ];
--- src/pages/_home/_sections/TechSkillsSection/index.tsx
+++ src/pages/_home/_sections/TechSkillsSection/index.tsx
@@ -3,39 +3,68 @@ import { addCustomId } from '#utils/addCustomId';
 import { TechnologyBlock } from '#components/TechnologyBlock';
 import { PageSection } from '#components/PageSection';
 import { useLocaleDictionary } from '#/hooks/useLocaleDictionary';
+import { Slider } from './Slider';
 import styles from './styles.module.scss';
+
 
 export function TechSkillsSection () {
   const { t } = useLocaleDictionary();
 
   const myTechSkills = technologies.map(addCustomId<typeof technologies[0]>);
 
-  const skillsToRender = myTechSkills.slice(0, 10).map((technology, index) => (
-    <li key={technology.customId}
-      className={styles.item}
-      style={{ '--position': index } as any}
-    >
-      <TechnologyBlock name={technology.name}
-        src={technology.imageSource}
-        loading="lazy"
-        decoding="auto"
-      />
-    </li>
-  ));
+  const frontendSkillItems = myTechSkills.reduce((items, technology) => {
+    const isfrontend = technology.category === 'frontend';
+    if (isfrontend) items.push(
+      <li key={technology.customId} className={styles.item}>
+        <TechnologyBlock name={technology.name}
+          src={technology.imageSource}
+          loading="eager"
+          decoding="sync"
+        />
+      </li>
+    );
+    return items;
+  }, []);
+
+  const backendSkillItems = myTechSkills.reduce((items, technology) => {
+    const isBackend = technology.category === 'backend';
+    if (isBackend) items.push(
+      <li key={technology.customId} className={styles.item}>
+        <TechnologyBlock name={technology.name}
+          src={technology.imageSource}
+          loading="eager"
+          decoding="sync"
+        />
+      </li>
+    );
+    return items;
+  }, []);
+
+  const myToolsItems = myTechSkills.reduce((items, technology) => {
+    const isTool = technology.category === 'tool';
+    if (isTool) items.push(
+      <li key={technology.customId} className={styles.item}>
+        <TechnologyBlock name={technology.name}
+          src={technology.imageSource}
+          loading="eager"
+          decoding="sync"
+        />
+      </li>
+    );
+    return items;
+  }, []);
 
   return (
     <PageSection whiteColor>
       <h2 className={styles.subtitle}>
         {t.what_i_use}
       </h2>
-      <div className={styles.slider}>
-        <ul className={styles.list} role="list">
-          {skillsToRender}
-        </ul>
-      </div>
-      {/* <ul className={styles.technologyGrid} role="list">
-        {skillsToRender}
-      </ul> */}
+      <h3 className={styles.label}>Frontend</h3>
+      <Slider duration={30}>{frontendSkillItems}</Slider>
+      <h3 className={styles.label}>Backend</h3>
+      <Slider duration={24} direction='right'>{backendSkillItems}</Slider>
+      <h3 className={styles.label}>Tools</h3>
+      <Slider>{myToolsItems}</Slider>
     </PageSection>
   );
 }
